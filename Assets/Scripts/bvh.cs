206c6d8eebd37f36bf40d76d10a10faa96e664d7
CCCpeggy <peggy19925@gmail.com>
2021-10-27 16:03:41 +0800

    合併 editor

--- Assets/Scripts/bvh.cs
+++ Assets/Scripts/bvh.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
+using UnityEditor;
+using UnityEditor.UIElements;
 
 [ExecuteInEditMode]
 public class bvh : MonoBehaviour
 {
     public List<BVH.BVHObject> BVHObjects = new List<BVH.BVHObject>();
-    void Start()
-    {
-        // BVH.BVHObject.CreateBVHObject(@"D:\workplace\3D遊戲\P1\bvh_sample_files\bvh_sample_files\walk_loop.bvh");
-    }
 
+}
+
+[CustomEditor(typeof(bvh))]
+public class bvhEditor : Editor
+{
+    string bvhFilePath = @"D:\workplace\3D遊戲\P1\bvh_sample_files\bvh_sample_files\walk_loop.bvh";
+    public override void OnInspectorGUI()
+    {
+        bvh myBvh = (bvh)target;
+        serializedObject.Update();
+        bvhFilePath = EditorGUILayout.TextField("Bvh File Path", bvhFilePath);
+        if(GUILayout.Button("Create"))
+        {
+            myBvh.BVHObjects.Add(BVH.BVHObject.CreateBVHObject(bvhFilePath).GetComponent<BVH.BVHObject>());
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
 }
